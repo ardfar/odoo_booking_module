22fed62c90b1e63090abf53da465909b51742e12
ardfar <me@aradenta.com>
2024-09-25 23:04:28 +0700

    add validation

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class ServiceTeam(models.Model):
     # Model info 
@@ -30,10 +31,23 @@ class SaleOrder(models.Model):
     
     @api.model
     def create(self, vals):
-        # Set booking order to True
-        vals['is_booking_order'] = True
-        # Call super
-        return super(SaleOrder, self).create(vals)
+        if 'prevent_save' in self._context:
+            return True
+        else:
+            # Set booking order to True
+            vals['is_booking_order'] = True
+            # Call super
+            return super(SaleOrder, self).create(vals)
+    
+    @api.model
+    def write(self, vals):
+        if 'prevent_save' in self._context:
+            return True
+        else:
+            # Set booking order to True
+            vals['is_booking_order'] = True
+            # Call super
+            return super().write(vals)
     
     @api.onchange('team')
     def _onchange_team(self):
@@ -43,6 +57,41 @@ class SaleOrder(models.Model):
         else:
             self.team_leader = False
             self.team_members = [(5, 0, 0)]
+    
+    @api.depends('team', 'booking_start', 'booking_end')
+    def action_check_booking_order(self):
+        
+        team = self.team
+        booking_start = self.booking_start
+        booking_end = self.booking_end
+        
+        if not team or not booking_start or not booking_end:
+            raise UserError(f"Please make sure to select a team and set booking dates")
+        
+        overlapping_work = self.env['booking_order_farras_arrafi_26092024.work_order'].search([
+            ('team', '=', team.id),
+            ('state', '=', 'in_progress'),
+            ('planned_start', '<=', booking_end),
+            ('planned_end', '>=', booking_start)
+        ])
+        
+        overlapping_work_ids = overlapping_work.ids
+        overlapping_work_names = self.env['booking_order_farras_arrafi_26092024.work_order'].browse(overlapping_work_ids).mapped('name')
+        
+        if overlapping_work:
+            raise UserError("Team already has work order during that period on {overlapping_work_names}")
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Success',
+                'message': 'The selected booking dates are available. You can proceed with saving the record.',
+                'sticky': False,
+            }
+        }
+    
+    
     
 class WorkOrder(models.Model):
     
